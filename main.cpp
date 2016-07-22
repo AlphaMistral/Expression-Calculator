d1eaa73046cba8654bb1b047343b4add53c81122
Yujingping <Mistral@weymire.com>
2016-07-22 15:34:28 +0800

    First Commit! Wish a good Start!

---
+++ main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  Numerical Analysis
+//
+//  Created by 于京平 on 16/7/22.
+//  Copyright © 2016年 于京平. All rights reserved.
+//
+
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main ()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
